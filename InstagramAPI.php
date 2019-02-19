b51a118a234658c7dbb04bc4745d5ee2940bcffb
Dusan <masinacdt@gmail.com>
2019-02-19 12:20:53 +0100

    instagram api class made

--- InstagramAPI.php
+++ InstagramAPI.php
@@ -1,17 +1,28 @@
 <?php
 
+require_once "settings.php";
+
 class Instagram
 {
 
+    private $client_id;
+    private $client_secret;
+    private $redirect_uri;
 
+    public function __construct($settings = array())
+    {
+        $this->client_id = $settings ['client_id']
+        $this->$client_secret = $settings['client_secret']
+        $this->redirect_uri = $settings['redirect_uri']
+    }
 
+    public function getLoginURL()
+    {
+      return "https://api.instagram.com/oauth/authorize/?client_id=$this->client_id&redirect_uri=$this->redirect_uri&response_type=code";
+    }
 
-  public function getLoginURL()
-  {
-      return "https://api.instagram.com/oauth/authorize/?client_id=CLIENT-ID&redirect_uri=REDIRECT-URI&response_type=code";
-  }
 }
 
-
+$instagram = new InstagramAPI ();
 
  ?>
